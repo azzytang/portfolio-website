b72049f246c1ad5a6f267c2a024aa4b73ca44cb1
Azalea Tang <azzytang@gmail.com>
2024-07-24 18:33:22 -0500

    added footer attributions

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -5,7 +5,15 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="Footer-attributions"></div>
+      <div className="Footer-attributions">
+        <div>
+          <a href="https://www.freepik.com/free-vector/abstract-background-with-hand-painted-clouds-design_50500233.htm#query=vintage%20sky&position=17&from_view=keyword&track=ais_user&uuid=819caba8-b60a-4686-8e6e-16fd202bcb4f">
+            Background Image by kjpargeter
+          </a>
+          on Freepik
+        </div>
+      </div>
+
       <div className="Footer-contact">
         <Link
           to="https://www.instagram.com/azzytang?igsh=MThnbHNrczBsY2F3&utm_source=qr"
